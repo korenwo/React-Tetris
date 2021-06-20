d02d0512f38b5afedfc092e23e8d4450623a0dd4
koren wohl <korenwohl@gmail.com>
2021-06-20 13:30:58 +0300

    first commit

---
+++ src/components/styles/StyledStartButton.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const StyledStartButton = styled.button`
+    box-sizing: border-box;
+    margin: 0 0 20px;
+    min-height : 30px;
+    width: 100%;
+    border-radius: 20px;
+    bprder: none;
+    color: white;
+    background: #333;
+    font-family: pixel, Arial, Helvetica, sans-serif;
+    font-size: 1rem;
+    outline: none;
+    cursor: pointer;
+`
