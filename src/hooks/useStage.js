633097e4e45e8a19774841ed23c674004400ba04
koren wohl <korenwohl@gmail.com>
2021-06-20 18:03:57 +0300

    change

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -10,7 +10,8 @@ export const useStage = (player, resetPlayer) => {
     const sweepRows = newStage =>
       newStage.reduce((ack, row) => {
         if (row.findIndex(cell => cell[0] === 0) === -1) {
-         
+          setRowsCleared(prev => prev + 1);
+          ack.unshift(new Array(newStage[0].length).fill([0, 'clear']));
           return ack;
         }
         ack.push(row);
